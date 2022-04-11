2bd7617eb35138b4e1125565015451bc9c309c57
Austinhs <austin@webglitch.com>
2022-04-11 00:00:15 -0400

    make things work all the time

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Getting started
 - Run `npm install`
 - Go to index.js modify `BASE_URI`, `START_ID`, `END_ID`, to your scrapping needs
-- Modify `CORES` to your CPU cores
-- You may need to modify `MAGIC_NUMBER` if you get a TCP connection issue, but I recommend leaving it as is first
+- Modify `WORKER_COUNT` based on how many process's you want to spawn. More will make it go faster if your computer can handle it.
 - To execute the script: `node index.js`
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import fs from 'fs';
 import { Pool } from 'multiprocessing';
 
-const CORES        = 8;
-const MAGIC_NUMBER = 8;
-const WORKER_COUNT = CORES * MAGIC_NUMBER;
+const WORKER_COUNT = 64;
 const WORKERS      = new Pool(WORKER_COUNT);
+const DIR_NAME     = 'metadata';
+const DIR_FILE_EXT = '';
+const FILE_NAME    = 'metadata_summary.json';
+const BASE_URI     = "https://divineanarchy.mypinata.cloud/ipfs/Qmaq9VcnrGQiBmGww1aWd6jLYbsgnczJQZdavyEku75LAE";
+const START_ID     = 1;
+const END_ID       = 5050;
 
 async function main() {
 	console.time();
-
-	const FILE_NAME = 'metadata.json';
-	const BASE_URI = "https://divineanarchy.mypinata.cloud/ipfs/Qmaq9VcnrGQiBmGww1aWd6jLYbsgnczJQZdavyEku75LAE";
-	const START_ID = 1;
-	const END_ID   = 5050;
 
 	if(!BASE_URI) {
 		console.log('Please pass an base uri when running this script after execution');
@@ -24,6 +23,12 @@ async function main() {
 		urls.push(`${BASE_URI}/${i}`);
 	}
 
+	// Create DIR to hold all metadata files
+	if(fs.existsSync(DIR_NAME)) {
+		fs.rmdirSync(DIR_NAME, { recursive: true });
+	}
+	fs.mkdirSync(DIR_NAME);
+
 	let worker_promises = [];
 	let worker_results  = [];
 	const chunk_amount    = Math.ceil(urls.length / WORKER_COUNT);
@@ -32,9 +37,16 @@ async function main() {
 		const chunk_urls = urls.slice(chunk_count, chunk_count + chunk_amount);
 		chunk_count += chunk_amount;
 
-		const promise = WORKERS.apply(chunk_urls, getMetadata, { onResult: result => worker_results = worker_results.concat(result) });
+		const promise = WORKERS.apply([chunk_urls, DIR_NAME, DIR_FILE_EXT], getMetadata, {
+			onResult: result => {
+				if(result instanceof Array) {
+					worker_results = worker_results.concat(result);
+				}
+			}
+		});
 		worker_promises.push(promise);
 	}
+
 	await Promise.all(worker_promises);
 
 	if(fs.existsSync(FILE_NAME)) {
@@ -46,32 +58,38 @@ async function main() {
 	console.timeEnd();
 }
 
-async function getMetadata(chunk_urls) {
-	const axios = require('axios');
+async function getMetadata(args) {
+	const axios    = require('axios');
+	const child_fs = require('fs');
 
-	if(chunk_urls.length == 0) {
-		return;
-	}
-
+	const [ chunk_urls, dir, ext ] = args;
 	console.log(`${process.pid} - Starting thread for: ${chunk_urls.length} requests`);
-	const updater_index = Math.floor(chunk_urls.length - 1 / 20);
 
 	let metadata = [];
 	for(const index in chunk_urls) {
-		try {
+		const loadMetadata = async () => {
 			const url      = chunk_urls[index];
-			const { data } = await axios.get(url);
+			const id       = url.split('/').pop();
+			const { data } = await axios.get(url, { timeout: 15000 });
 
-			if(index % updater_index == 0) {
-				console.log(`${process.pid} - On index: ${index} / ${chunk_urls.length - 1}`);
-			}
-
+			child_fs.writeFileSync(`${dir}/${id}${ext}`, JSON.stringify(data, null, 2));
 			metadata.push(data);
+		}
+
+		try {
+			await loadMetadata();
 		} catch(err) {
-			console.log(err.message);
+			console.log('Shit broke, but we will try again just incase');
+
+			try {
+				await loadMetadata();
+			} catch(err) {
+				console.log(err.message);
+			}
 		}
 	}
 
+	console.log(`Finished with pid: ${process.pid}`);
 	return metadata;
 }
 
